72062c41d872551061feaccd2aa7e0a1a0633161
Quang <bnnquang@gmail.com>
2021-12-22 15:08:35 +0000

    The second change

--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from .models import Todolist 
+from django.shortcuts import render, redirect
+
+from .models import Todolist
+
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items' : todo_items}
-    return render(request, 'todolist/index.html', context)
+    form = TodoListForm()
+    context = {'todo_items' :todo_items, 'form' : form}
+    return render(request,'todolist/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+    print(request.POST['text'])
+    return redirect('index')
